f6406a76e129872cc728155eed13562341cb9f89
iman-sami <imansamifaith@gmail.com>
2022-10-23 21:43:45 +0300

    middleware added

---
+++ middleware/errorHandler.js
@@ -0,0 +1,20 @@
+import serverError  from "../config/serverError";
+
+function sendError(err,res){
+    res.status(err.statusCode).json({
+        status:err.status,
+        message:err.message,
+        error:err,
+        stack:err.stack
+    })
+}
+
+export function CatchGlobalError(err,req,res,next){
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status
+     sendError(err,res)
+}
+
+export function catchAsync(fn){
+    return (req,res,next)=> fn(req,res,next).catch(next)
+}
