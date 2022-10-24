3d89d650a5d0c19860b67e928e3cab7e23899da8
iman-sami <imansamifaith@gmail.com>
2022-10-24 21:51:29 +0300

    app.js updated

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ import express  from "express";
 import cors from 'cors';
 import helmet from 'helmet';
 import { db } from "./config/connection";
-
+import routes from "./routes/index.js";
 const app = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded());
+routes(app)
 db();
 
 
