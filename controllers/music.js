5177f77b02fd9fe5bebfc6499aa2e9ec65a54b0c
iman-sami <imansamifaith@gmail.com>
2022-10-26 20:56:36 +0300

    music controller added

--- controllers/music.js
+++ controllers/music.js
@@ -1,11 +1,28 @@
 import { catchAsync } from "../middleware/errorHandler";
-
+import music from "../models/music.js";
+import serverError  from "../config/serverError";
+import { json } from "express";
 export const MusicController = {
- 
+    // all music
     allMusics: catchAsync(async (req,res,next)=>{
-        
-    })
-
-
+        music.find()
+        .populate("album")
+        .populate("genre")
+        .exec((err,results)=>{
+            if (err) return next(new serverError(err.message,404))
+            res.json(results)
+        })
+    }),
+    // selected music
+    selectedMusic:catchAsync(async(req,res,next)=>{
+        const id = req.params.id
+        music.findOne({_id:id})
+        .populate("album")
+        .populate("genre")
+        .exec((err,results)=>{
+            if (err) return next(new serverError(err.message,404))
+            res.json(results)
+        })
+    }),
 
 }
