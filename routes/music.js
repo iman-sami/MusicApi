2761aa5fa1d1a788d8740733be31b5850082c517
iman-sami <imansamifaith@gmail.com>
2022-10-28 05:26:01 +0300

    music route updated

--- routes/music.js
+++ routes/music.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { MusicController } from "../controllers/music.js";
+import upload from "../utils/multer.js";
 
 const router = Router()
 
 router.route("/music").get(MusicController.allMusics)
 router.route("/music/:id").get(MusicController.selectedMusic)
+router.route("/music/upload").post(upload.array("musics"),MusicController.uploadMusic)
 
 
 export default router;
