6ca662039ddfa43c5c6a594a566abf51463ad801
iman-sami <imansamifaith@gmail.com>
2022-11-03 00:00:11 +0300

    app.js updated

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ import express  from "express";
 import cors from 'cors';
 import helmet from 'helmet';
 import { db } from "./config/connection.js";
-import routes from "./routes/index.js";
+import index from "./routes/index.js";
+import { PORT } from "./config/constant.js";
 const app = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded());
-routes(app)
+app.get("/",(req,res)=>{
+    res.json({message:"hello"})
+})
+index(app)
+
 db();
 
 
@@ -17,6 +22,6 @@ db();
 
 
 
-app.listen(3000, () => {
-    console.log('App listening on port 3000!');
+app.listen(PORT, () => {
+    console.log('App listening on port '+ PORT);
 });
