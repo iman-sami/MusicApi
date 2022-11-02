093b2a6be9b84d14d84ab486049081de29debdf0
iman-sami <imansamifaith@gmail.com>
2022-11-03 00:00:00 +0300

    index updated

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ import { CatchGlobalError } from "../middleware/errorHandler.js"
 import auth from "./auth.js"
 import music from "./music.js"
 export default (app) =>{
+
+    
     app.use("/auth",auth)
-    app.use("/user")
+
+    // app.use("/user")
     app.use("/music",music)
+
     app.use(CatchGlobalError)
 }
